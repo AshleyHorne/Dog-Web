8c400723bc67ca0747c3d14bfcccba604a859cda
Ash <ashleyhorne348@yahoo.com>
2019-05-16 14:01:00 -0400

    navigation and backdrop

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -4,11 +4,11 @@ import profile from '../../Asset/Image/dog-picture.jpg';
 import classes from './Logo.css';
 
 
-const Logo = (props) => {
+const Logo = (props) => (
     <div className={classes.Logo}>
         <img src = {profile} alt="Icon"/>
     </div>
 
-};
+);
 
 export default Logo;
